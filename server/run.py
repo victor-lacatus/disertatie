2f433f9102a1656bb2852936f43bb3a3f7b116e3
radu-pythia <radu@pythia.social>
2022-07-17 14:17:13 +0300

    added dowload

--- server/run.py
+++ server/run.py
@@ -1,9 +1,12 @@
-
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from models import DataSet, db, User
 from bson.objectid import ObjectId
+from io import BytesIO
 
 app = FastAPI()
+app2 = FastAPI()
+
 
 @app.get('/users')
 async def list_users():
@@ -18,8 +21,7 @@ async def get_user(key, value):
         value = ObjectId(value)
     user = db.users.find_one({key : value})
     if user is not None:
-        user = { "id" if k == "_id" else k:v for k,v in user.items() }
-        user["id"] = str(user["id"])   
+        user = User(**user)   
         return user
     return {"error": "The user with {key} = {value} does not exist!}"}
 
@@ -52,14 +54,14 @@ async def get_datasets():
 
 
 
-@app.post('/user/{email}')
+@app.delete('/user/{email}')
 async def delete_user(email):
     ret = db.users.delete_one({"email" : email})
     ret = { "id" if k == "_id" else k:v for k,v in ret.items() }
     ret["id"] = str(ret["id"])
     return ret
 
-@app.post('/dataset/{key}={value}')
+@app.delete('/dataset/{key}={value}')
 async def delete_dataset(key, value):
     if key == 'id' or key == '_id':
         ret = db.datasets.delete_one({"_id" : ObjectId(id)})
@@ -68,3 +70,16 @@ async def delete_dataset(key, value):
     ret = { "id" if k == "_id" else k:v for k,v in ret.items() }
     ret["id"] = str(ret["id"])
     return ret
+
+@app.get("/dataset/download/{id}")
+def download_fle(id):
+    
+    ret = db.datasets.find_one({"_id" : ObjectId(id)})
+    print(ret)
+    if ret is None:
+        return {"error": "No such file!"}
+    ret["_id"] = str(ret["_id"])
+    response = JSONResponse(ret)
+    response.headers["Content-Disposition"] = "attachment; filename=export.json"
+    return response
+
---
+++ test.py
@@ -0,0 +1,10 @@
+demolist = ['strig', 34, 88, False]
+print(demolist)
+
+demolist.append("ion")
+
+print(demolist)
+
+demolist.pop(2)
+print(demolist)
+
